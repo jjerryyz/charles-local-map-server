a281d89fe239ec8d53dbb649b51452e12429de9e
jerryzheng <zheng.jianli@outlook.com>
2021-02-10 17:54:41 +0800

    refactor: formate request file path

--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,36 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 const arg = require('arg');
+const os = require('os');
 
 const args = arg({'--path': String});
 
-const MOCK_LOCAL_FILES_PATH = args['--path'] ? args['--path'].replace(/\\/g, '\/') : '';
+const MOCK_LOCAL_FILES_PATH = args['--path'] || args['_'].shift() || '';
+
+if (!MOCK_LOCAL_FILES_PATH) {
+  console.error('please specific local map files');
+}
 
 router.all('/*', function(req, res, next) {
 
-  console.log('req', req.query, req.path);
+  // console.log('req', req.query, req.path);
   let fileQueryStr = Object.keys(req.query).reduce((prev, current) => {
     const value = req.query[current];
     if (!!prev) prev += '&'
     return `${prev}${current}=${value}`;
   }, '');
 
+  let reqPath = req.path;
+  if (os.platform() === 'win32') {
+    reqPath = reqPath.replace(/\\/g, '\/');
+  }
+
   let filePath;
   if (!!fileQueryStr) {
     fileQueryStr = '?' + fileQueryStr;
-    filePath = req.path + encodeURIComponent(fileQueryStr);
+    filePath = reqPath + encodeURIComponent(fileQueryStr);
   } else {
-    filePath = req.path;
+    filePath = reqPath;
   }
   filePath = MOCK_LOCAL_FILES_PATH + filePath
   // console.log('filePath', filePath);
@@ -33,7 +43,7 @@ router.all('/*', function(req, res, next) {
       res.status(200).json(JSON.parse(data));
     } else {
       res.end();
-      console.log('read', err, data);
+      // console.log('read', err, data);
     }
   })
 });
